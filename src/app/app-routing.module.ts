51b3ade9b99cf4d8736a49bd5d3eaaba32f2da72
Raphael Anizio da Silva <raphaelanizio01@gmail.com>
2024-02-01 21:25:48 -0300

    iniciando projeto com angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-const routes: Routes = [];
+// mapeando determinados caminhos de URL para componentes específicos
+const routes: Routes = [
+  // definindo o caminho da URL que a rota deve corresponder e permitindo que os componentes associados às rotas sejam exibidos em locais específicos do seu aplicativo conforme a navegação ocorre
+  {path: '', component: HomeComponent}, // pagina home
+  {path: 'about', component: AboutComponent} // pagina about
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
